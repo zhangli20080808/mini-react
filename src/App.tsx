93a1c6ebca785a4c2e0d75e636885f8a861cab03
zhangli20080808 <791492434@qq.com>
2022-09-25 14:45:52 +0800

    ts版本,后续学习使用jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
+import { useState } from 'react';
+import { Button, Input, Icon, Menu, AutoComplete } from 'design-component';
+export const BCustomComplete = (args) => {
+  const lakersWithNumber = [
+    { value: 'bradley', number: 11 },
+    { value: 'pope', number: 1 },
+    { value: 'caruso', number: 4 },
+    { value: 'cook', number: 2 },
+    { value: 'cousins', number: 15 },
+    { value: 'james', number: 23 },
+    { value: 'AD', number: 3 },
+    { value: 'green', number: 14 },
+    { value: 'howard', number: 39 },
+    { value: 'kuzma', number: 0 },
+  ];
+  const handleFetch = (query: string) => {
+    return lakersWithNumber.filter((player) => player.value.includes(query));
+  };
+  const renderOption = (item) => {
+    const itemWithNumber = item;
+    return (
+      <>
+        <b>名字: {itemWithNumber.value}</b>
+        <span>球衣号码: {itemWithNumber.number}</span>
+      </>
+    );
+  };
+  return (
+    <AutoComplete
+      {...args}
+      fetchSuggestions={handleFetch}
+      placeholder='输入湖人队球员英文,自定义下拉模版'
+      renderOption={renderOption}
+    />
+  );
+};
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is {count}
+      </Button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Input />
+        <Icon icon='adjust' size='2x' />
+        <Menu defaultIndex='0'>
+          <Menu.Item>cool link</Menu.Item>
+          <Menu.Item>cool link 2</Menu.Item>
+          <Menu.Item disabled>disabled</Menu.Item>
+          <Menu.SubMenu title='下拉选项'>
+            <Menu.Item>下拉选项一</Menu.Item>
+            <Menu.Item>下拉选项二</Menu.Item>
+          </Menu.SubMenu>
+        </Menu>
+        <BCustomComplete />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
